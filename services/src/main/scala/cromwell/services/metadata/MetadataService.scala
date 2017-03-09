2b69fd8da49de6afa920840ee23166e05f3db6c5
Chris Llanwarne <cjllanwarne@gmail.com>
2017-03-09 13:34:03 -0500

    Workflow name length limit

--- services/src/main/scala/cromwell/services/metadata/MetadataService.scala
+++ services/src/main/scala/cromwell/services/metadata/MetadataService.scala
@@ -6,7 +6,7 @@ import akka.actor.ActorRef
 import cats.data.NonEmptyList
 import cromwell.core.{FullyQualifiedName, JobKey, WorkflowId, WorkflowState}
 import cromwell.services.ServiceRegistryActor.ServiceRegistryMessage
-import lenthall.exception.ThrowableAggregation
+import lenthall.exception.{MessageAggregation, ThrowableAggregation}
 import wdl4s.values._
 
 import scala.util.Random
@@ -143,7 +143,19 @@ object MetadataService {
         } else {
           emptyCauseList
         }
-
+        message ++ indexedCauseEvents
+      case aggregation: MessageAggregation =>
+        val message = List(MetadataEvent(metadataKey.copy(key = s"${metadataKey.key}[$failureIndex]:message"), MetadataValue(aggregation.exceptionContext)))
+        val indexedCauses = aggregation.errorMessages.toList.zipWithIndex
+        val indexedCauseEvents = if (indexedCauses.nonEmpty) {
+          indexedCauses flatMap { case (cause, index) =>
+            val causeMessageKey = metadataKey.copy(key = metadataKey.key + s"[$failureIndex]:causedBy[$index]:message")
+            val causeCausedByKey = metadataKey.copy(key = metadataKey.key + s"[$failureIndex]:causedBy[$index]:causedBy[]")
+            List(MetadataEvent(causeMessageKey, MetadataValue(cause)), MetadataEvent.empty(causeCausedByKey))
+          }
+        } else {
+          emptyCauseList
+        }
         message ++ indexedCauseEvents
       case other =>
         val message = List(MetadataEvent(metadataKey.copy(key = s"${metadataKey.key}[$failureIndex]:message"), MetadataValue(t.getMessage)))
